7e8e8f37e7b46a148906f7b182f101b44ab86b63
JonathanBell09 <jonny.s.bell@gmail.com>
2023-12-05 15:32:19 +0100

    Updated NEB Questions - Fund4ACT

--- Fund4Act/services/new-european-bauhaus.js
+++ Fund4Act/services/new-european-bauhaus.js
@@ -1,20 +1,17 @@
 module.exports.getAmbitionLevels = async ({beautyAmbitionInput, sustainAmbitionInput, togetherAmbitionInput,
-  participProcessAmbitionInput, transdiciplinaryAmbitionInput, multiLevelEngagementAmbitionInput}) => {
+  participAmbitionInput, transdiciplinaryAmbitionInput, multiLevelEngagementAmbitionInput}) => {
 
   var beautyAmbition;
   if(!Array.isArray(beautyAmbitionInput)) {
     beautyAmbitionInput = [beautyAmbitionInput];
   }
-  if (beautyAmbitionInput.includes("integrate_1") || beautyAmbitionInput.includes("integrate_2") ||
-  beautyAmbitionInput.includes("integrate_3") || beautyAmbitionInput.includes("integrate_4")) {
+  if (beautyAmbitionInput.includes("integrate")) {
     beautyAmbition = 3;
   }
-  else if (beautyAmbitionInput.includes("connect_1") || beautyAmbitionInput.includes("connect_2") ||
-  beautyAmbitionInput.includes("connect_3") || beautyAmbitionInput.includes("connect_4")) {
+  else if (beautyAmbitionInput.includes("connect")) {
     beautyAmbition = 2;
   }
-  else if (beautyAmbitionInput.includes("activate_1") || beautyAmbitionInput.includes("activate_2") ||
-  beautyAmbitionInput.includes("activate_3") || beautyAmbitionInput.includes("activate_4")) {
+  else if (beautyAmbitionInput.includes("activate")) {
     beautyAmbition = 1;
   }
   else {
@@ -25,18 +22,13 @@ module.exports.getAmbitionLevels = async ({beautyAmbitionInput, sustainAmbitionI
   if(!Array.isArray(sustainAmbitionInput)) {
     sustainAmbitionInput = [sustainAmbitionInput];
   }
-  if (sustainAmbitionInput.includes("regenerate_1") || sustainAmbitionInput.includes("regenerate_2") ||
-  sustainAmbitionInput.includes("regenerate_3")) {
+  if (sustainAmbitionInput.includes("regenerate")) {
     sustainAmbition = 3;
   }
-  else if (sustainAmbitionInput.includes("close_loop_1") || sustainAmbitionInput.includes("close_loop_2") ||
-  sustainAmbitionInput.includes("close_loop_3") || sustainAmbitionInput.includes("close_loop_4") ||
-  sustainAmbitionInput.includes("close_loop_5")) {
+  else if (sustainAmbitionInput.includes("close_loop")) {
     sustainAmbition = 2;
   }
-  else if (sustainAmbitionInput.includes("repurpose_1") || sustainAmbitionInput.includes("repurpose_2") ||
-  sustainAmbitionInput.includes("repurpose_3") || sustainAmbitionInput.includes("repurpose_4") ||
-  sustainAmbitionInput.includes("repurpose_5")) {
+  else if (sustainAmbitionInput.includes("repurpose")) {
     sustainAmbition = 1;
   }
   else {
@@ -47,15 +39,13 @@ module.exports.getAmbitionLevels = async ({beautyAmbitionInput, sustainAmbitionI
   if(!Array.isArray(togetherAmbitionInput)) {
     togetherAmbitionInput = [togetherAmbitionInput];
   }
-  if (togetherAmbitionInput.includes("transform_1") || togetherAmbitionInput.includes("transform_2")) {
+  if (togetherAmbitionInput.includes("transform")) {
     togetherAmbition = 3;
   }
-  else if (togetherAmbitionInput.includes("consolidate_1") || togetherAmbitionInput.includes("consolidate_2") ||
-  togetherAmbitionInput.includes("consolidate_3")) {
+  else if (togetherAmbitionInput.includes("consolidate")) {
     togetherAmbition = 2;
   }
-  else if (togetherAmbitionInput.includes("include_1") || togetherAmbitionInput.includes("include_2") || 
-  togetherAmbitionInput.includes("include_3")) {
+  else if (togetherAmbitionInput.includes("include")) {
     togetherAmbition = 1;
   }
   else {
@@ -63,19 +53,16 @@ module.exports.getAmbitionLevels = async ({beautyAmbitionInput, sustainAmbitionI
   }
 
   var participProcessAmbition;
-  if(!Array.isArray(participProcessAmbitionInput)) {
-    participProcessAmbitionInput = [participProcessAmbitionInput];
+  if(!Array.isArray(participAmbitionInput)) {
+    participProcessAmbitionInput = [participAmbitionInput];
   }
-  if (participProcessAmbitionInput.includes("self-govern_1") || participProcessAmbitionInput.includes("self-govern_2") ||
-  participProcessAmbitionInput.includes("self-govern_3") || participProcessAmbitionInput.includes("self-govern_4")) {
+  if (participAmbitionInput.includes("self-govern")) {
     participProcessAmbition = 3;
   }
-  else if (participProcessAmbitionInput.includes("co-develop_1") || participProcessAmbitionInput.includes("co-develop_2") ||
-  participProcessAmbitionInput.includes("co-develop_3") || participProcessAmbitionInput.includes("co-develop_4")) {
+  else if (participAmbitionInput.includes("co-develop")) {
     participProcessAmbition = 2;
   }
-  else if (participProcessAmbitionInput.includes("consult_1") || participProcessAmbitionInput.includes("consult_2") ||
-  participProcessAmbitionInput.includes("consult_3")) {
+  else if (participAmbitionInput.includes("consult")) {
     participProcessAmbition = 1;
   }
   else {
@@ -86,15 +73,13 @@ module.exports.getAmbitionLevels = async ({beautyAmbitionInput, sustainAmbitionI
   if(!Array.isArray(multiLevelEngagementAmbitionInput)) {
     multiLevelEngagementAmbitionInput = [multiLevelEngagementAmbitionInput];
   }
-  if (multiLevelEngagementAmbitionInput.includes("work_globally_1") || multiLevelEngagementAmbitionInput.includes("work_globally_2")) {
+  if (multiLevelEngagementAmbitionInput.includes("work_globally")) {
     multiLevelEngagementAmbition = 3;
   }
-  else if (multiLevelEngagementAmbitionInput.includes("work_across_1") || multiLevelEngagementAmbitionInput.includes("work_across_2") ||
-  multiLevelEngagementAmbitionInput.includes("work_across_3")) {
+  else if (multiLevelEngagementAmbitionInput.includes("work_across")) {
     multiLevelEngagementAmbition = 2;
   }
-  else if (multiLevelEngagementAmbitionInput.includes("work_locally_1") || multiLevelEngagementAmbitionInput.includes("work_locally_2") ||
-  multiLevelEngagementAmbitionInput.includes("work_locally_3")) {
+  else if (multiLevelEngagementAmbitionInput.includes("work_locally")) {
     multiLevelEngagementAmbition = 1;
   }
   else {
@@ -105,16 +90,13 @@ module.exports.getAmbitionLevels = async ({beautyAmbitionInput, sustainAmbitionI
   if(!Array.isArray(transdiciplinaryAmbitionInput)) {
     transdiciplinaryAmbitionInput = [transdiciplinaryAmbitionInput];
   }
-  if (transdiciplinaryAmbitionInput.includes("beyond_disciplinary_1") || transdiciplinaryAmbitionInput.includes("beyond_disciplinary_1") ||
-  transdiciplinaryAmbitionInput.includes("beyond_disciplinary_3") || transdiciplinaryAmbitionInput.includes("beyond_disciplinary_4")) {
+  if (transdiciplinaryAmbitionInput.includes("beyond_disciplinary")) {
     transdiciplinaryAmbition = 3;
   }
-  else if (transdiciplinaryAmbitionInput.includes("interdisciplinary_1") || transdiciplinaryAmbitionInput.includes("interdisciplinary_2") ||
-  transdiciplinaryAmbitionInput.includes("interdisciplinary_3")) {
+  else if (transdiciplinaryAmbitionInput.includes("interdisciplinary")) {
     transdiciplinaryAmbition = 2;
   }
-  else if (transdiciplinaryAmbitionInput.includes("multidisciplinary_1") || transdiciplinaryAmbitionInput.includes("multidisciplinary_2") ||
-  transdiciplinaryAmbitionInput.includes("multidisciplinary_3") || transdiciplinaryAmbitionInput.includes("multidisciplinary_4")) {
+  else if (transdiciplinaryAmbitionInput.includes("multidisciplinary")) {
     transdiciplinaryAmbition = 1;
   }
   else {
