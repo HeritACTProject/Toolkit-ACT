bb4b80603fc7b8a2b5ed3a10f791be8ffeef65b4
corsada <james@corscada.uk>
2025-04-09 11:50:31 +0100

    adds edit profile flow

--- Fund4Act/controllers/action-creator-update.js
+++ Fund4Act/controllers/action-creator-update.js
@@ -4,25 +4,25 @@ const profile = require('../models/profiles.js');
 exports.post = [
   [
     body('logo-url').trim().escape(),
-    body('website-url').trim().escape(),
+    body('website-url').isURL().trim(),
     body('mission').notEmpty().trim().escape(),
     body('prev-actions').trim().escape(),
     body('prev-grants').trim().escape(),
     body('partnerships').notEmpty().escape(),
-    body('constitution-url').notEmpty().trim().escape(),
-    body('cap-url').notEmpty().trim().escape(),
+    body('constitution-url').notEmpty().isURL().trim(),
+    body('cap-url').isURL().trim(),
   ], async (req, res, next) => {
     const formData = {
       id: req.user.id,
       email: req.body.email,
       logoUrl: req.body['logo-url'],
-      websiteUrl: req.body['website-url'],
+      websiteUrl: encodeURIComponent(req.body['website-url']),
       mission: req.body.mission,
       prevActions: req.body['prev-actions'],
       prevGrants: req.body['prev-grants'],
       partnerships: req.body.partnerships,
-      constitutionUrl: req.body['constitution-url'],
-      climateActionPlanUrl: req.body['cap-url'],
+      constitutionUrl: encodeURIComponent(req.body['constitution-url']),
+      climateActionPlanUrl: encodeURIComponent(req.body['cap-url']),
     };
 
     try {
