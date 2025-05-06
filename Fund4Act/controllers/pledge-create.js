9b34c9e15de8b77b9846981199ede9f149ebd5b7
corsada <james@corscada.uk>
2025-05-06 17:17:16 +0100

    adds email notification to pledges

--- Fund4Act/controllers/pledge-create.js
+++ Fund4Act/controllers/pledge-create.js
@@ -1,10 +1,13 @@
 const { param, body, validationResult } = require('express-validator');
-const pledge = require('../models/pledges.js');
+const { sendMail } = require('../tools/email-service');
+const pledge = require('../models/pledges');
+const profile = require('../models/profiles');
+const action = require('../models/actions');
 
 exports.post = [
   [
     body('amount').notEmpty().isNumeric(),
-    body('comment').notEmpty(),
+    body('comment'),
   ], async (req, res, next) => {
     const data = {
       proj_slug: req.params.slug,
@@ -21,6 +24,15 @@ exports.post = [
     }
     try {
       await pledge.create(data);
+      const actionInfo = await action.getByActionWithOwnerInfo(data.proj_slug)[0];
+      const donorInfo = await profile.getProfileInfo(req.user.id);
+      const subject = `${actionInfo.name} has recieved a new pledge`;
+      const text = `A new pledge of €${data.amount} was recieved from ${donorInfo.display_name}. `
+        + `Contact ${donorInfo.email} to arrange payment.`;
+      let html = `<p>A new pledge of €${data.amount} was recieved from ${donorInfo.display_name}.`;
+      if (data.comment) html += `<p>They said: "${data.comment}"`;
+      html += `<p>Contact ${donorInfo.email} to arrange payment.`;
+      sendMail(actionInfo.owner_email, 'pledges@fund4act.com', subject, text, html);
     } catch (err) {
       res.status(500).json({ errors: err.array });
       next(err);
